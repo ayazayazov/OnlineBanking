c5f0e12e7de690ff1403ad93589c65fc8b9ae2dd
ayazayazov <ayazayazov00@gmail.com>
2023-12-03 03:38:46 +0400

    login page created

---
+++ index.js
@@ -0,0 +1,25 @@
+let emailInput = document.getElementById('emailInput');
+let passwordInput = document.getElementById('passwordInput');
+const logInBtn = document.getElementById('logInBtn');
+const eyeOnOff = document.getElementById('eyeOnOff');
+
+logInBtn.addEventListener('click', function(){
+    console.log('click');
+    if(emailInput.value === '' || passwordInput.value === ''){
+        alert('Please type the email or password.');
+    }else{
+        window.location.href = './html/dashboard.html';
+    }
+})
+
+eyeOnOff.addEventListener('click', ()=>{
+    if(passwordInput.type === 'password'){
+        passwordInput.type = 'text';
+        eyeOnOff.classList.remove('eyeOff');
+        eyeOnOff.classList.add('eyeOn');
+    }else{
+        passwordInput.type = 'password';
+        eyeOnOff.classList.remove('eyeOn');
+        eyeOnOff.classList.add('eyeOff');
+    }
+})
